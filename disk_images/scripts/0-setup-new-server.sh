7260edb76f1c20a2ec3411d5d3e3b2c3f49d2efe
Ubuntu <ubuntu@ip-172-31-23-239.ec2.internal>
2025-12-13 01:46:19 +0000

    add setup for docker

--- disk_images/scripts/0-setup-new-server.sh
+++ disk_images/scripts/0-setup-new-server.sh
@@ -12,5 +12,15 @@ sudo apt install -y bridge-utils
 sudo addgroup libvirtd
 sudo adduser ubuntu libvirtd
 
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl status docker
+
+
+
+
 git clone https://github.com/pongstorn-AMD/pickle-resources.git
 
